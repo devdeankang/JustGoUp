9ebbd614dc1e0fe3d860ab9fb8ea072175f3abe5
deankang <devdeankang@gmail.com>
2024-09-19 04:47:08 +0900

    장애물 전략 패턴 : Push, Swing, Spike, Disappear 전략 구현

---
+++ Assets/Scripts/Obstacle/DisappearStrategy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class DisappearStrategy : IObstacleStrategy
+{
+    float disappearTime;
+    float timer;
+
+    public DisappearStrategy(float disappearTime)
+    {
+        this.disappearTime = disappearTime;
+        this.timer = 0f;
+    }
+
+    public void Execute(ObstacleController obstacle)
+    {
+        timer += Time.deltaTime;
+        if(timer >= disappearTime)
+        {
+            //@@ Consider using a PoolManager to handle object pooling. 
+            obstacle.gameObject.SetActive(false);
+        }
+    }
+    
+}
