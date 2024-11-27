cd4cc165b1bd9f535f6404775ef9dcdf39dd8428
deankang <devdeankang@gmail.com>
2024-11-28 07:44:39 +0900

    딕셔너리 기반 매핑을 활용한 StrategyFactory 구현 및 ScriptableObject Config 추가

---
+++ Assets/Scripts/Obstacle/Configs/SwingConfig.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SwingConfig", menuName = "Obstacle/SwingConfig")]
+public class SwingConfig : StrategyConfig
+{
+    public float swingSpeed;
+    public float swingAngle;
+    public float swingForce;
+}
---
+++ Assets/Scripts/Obstacle/StrategyFactory.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class StrategyFactory
+{
+    static Dictionary<Type, Func<StrategyConfig, IObstacleStrategy>> strategyMap = 
+        new Dictionary<Type, Func<StrategyConfig, IObstacleStrategy>>();
+
+    static StrategyFactory()
+    {
+        RegisterStrategy<SpikesConfig>(config => new SpikesStrategy(config as SpikesConfig));
+        RegisterStrategy<SwingConfig>(config => new SwingStrategy(config as SwingConfig));
+        RegisterStrategy<PushConfig>(config => new PushStrategy(config as PushConfig));
+
+        RegisterStrategy<TimeLimitedConfig>(config =>
+        {
+            var timeLimitedConfig = config as TimeLimitedConfig;
+            var innerStrategy = CreateStrategy(timeLimitedConfig.innerStrategyConfig);
+            return new TimeLimitedStrategy(innerStrategy, timeLimitedConfig.timeLimit);
+        });
+    }
+
+    public static void RegisterStrategy<T>(Func<StrategyConfig, IObstacleStrategy> factoryMethod) where T : StrategyConfig
+    {
+        strategyMap[typeof(T)] = factoryMethod;
+    }
+
+    public static IObstacleStrategy CreateStrategy(StrategyConfig config)
+    {
+        if (config == null) return null;
+
+        var configType = config.GetType();
+        if (strategyMap.TryGetValue(configType, out var factoryMethod))
+        {
+            return factoryMethod(config);
+        }
+
+        Debug.LogWarning($"StrategyConfig Type is not registered : {configType}");
+        return null;
+    }
+
+    public static TimeLimitedConfig CreateTimeLimitedConfig(StrategyConfig innerConfig, float timeLimit)
+    {
+        TimeLimitedConfig config = ScriptableObject.CreateInstance<TimeLimitedConfig>();
+        config.innerStrategyConfig = innerConfig;
+        config.timeLimit = timeLimit;
+        return config;
+    }
+}
